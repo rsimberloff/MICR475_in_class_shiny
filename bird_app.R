0241143a62826f0549559df3ae61c91ed7f618c1
Ruth Simberloff <rsimberloff@gmail.com>
2021-11-12 11:46:02 -0500

    Annotated Shiny code

--- bird_app.R
+++ bird_app.R
@@ -4,19 +4,20 @@ library(ggmap)
 WSKO <- read.csv("territorymapexample.csv") ## location data from WS/KO, one sparrow next to the Golden Gate Bridge
 register_google(key = "AIzaSyAinMZGtkgpmpXIYJguj5K08Ja0sBv6cr4") ## register API key to access Google maps through ggmap
 
+# Designing User Interface of Shiny App
 ui <- fluidPage(
-                titlePanel("Kernal Density Home Range of a Specific Sparrow"),
-                sliderInput(inputId = "h",
+                titlePanel("Kernal Density Home Range of a Specific Sparrow"), #Adding title panel
+                sliderInput(inputId = "h", #Adding slider scale based on user input h
                             label = "Kernal Width (h)",
-                            min = 0.00005,
+                            min = 0.00005, #Setting max and min kernel width values
                             max = 0.001,
                             value = 0),
-                plotOutput(outputId = "densityplot")
+                plotOutput(outputId = "densityplot") #Setting output to plot
                 #submitButton(text = "New Plot")
 
 )
 
-
+# Make the plot
 server <- function(input, output){
   output$densityplot <- renderPlot({
     map <- ggmap(get_googlemap(center = c(lon = -122.477087, lat = 37.80741702), ## get a base map from Google, specify map center
